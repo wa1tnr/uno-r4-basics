3eb1c71ba9c68835e2e56329cb88b2ec4ca617a4
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-19 02:13:08 +0000

    Solid l1 thru l6 added to dictionary
      l1 is shorthand for the LED at location '1' in the schematic.
      l1 is upper left corner and they march across L to R.
    
    	modified:   dict.h
    	modified:   matrix.h
    
    On branch dvlp-aa

--- test-sketches.d/sketch-cc/src/matrix.h
+++ test-sketches.d/sketch-cc/src/matrix.h
@@ -1,7 +1,12 @@
 // matrix.h
 
-NAMED(_l1d, "l1");
-// l1 3L 7H
-void l1d() { int clr = 3; int setb = 7; push(clr);  acld(); push(setb); asbd(); }
+#define write_register() push(clr); acld(); push(setb); asbd();
+
+NAMED(_l1d, "l1"); void l1d() { int clr = 3; int setb = 7; write_register(); }
+NAMED(_l2d, "l2"); void l2d() { int clr = 7; int setb = 3; push(clr);  acld(); push(setb); asbd(); }
+NAMED(_l3d, "l3"); void l3d() { int clr = 4; int setb = 7; push(clr);  acld(); push(setb); asbd(); }
+NAMED(_l4d, "l4"); void l4d() { int clr = 7; int setb = 4; push(clr);  acld(); push(setb); asbd(); }
+NAMED(_l5d, "l5"); void l5d() { int clr = 4; int setb = 3; push(clr);  acld(); push(setb); asbd(); }
+NAMED(_l6d, "l6"); void l6d() { int clr = 3; int setb = 4; push(clr);  acld(); push(setb); asbd(); }
 
 /* END */
