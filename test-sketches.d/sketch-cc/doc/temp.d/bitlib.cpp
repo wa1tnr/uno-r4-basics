1c86805bb53989659a0124d4028dc6ab32db5517
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-20 05:54:01 +0000

    nice snapshot of program
    	modified:   bitlib.cpp
    
    On branch dvlp-aa

--- test-sketches.d/sketch-cc/doc/temp.d/bitlib.cpp
+++ test-sketches.d/sketch-cc/doc/temp.d/bitlib.cpp
@@ -3,15 +3,16 @@
 
 static const uint16_t binPins[] = {0xF000, 0xF001, 0xF002, 0xF003};
 
-// from arduino.cc github led matrix example .h file:
 static const uint8_t pins[][2] = {
     // anode, cathode
     {7, 3},  {3, 7},  {7, 4},  {4, 7},  {3, 4},  {4, 3},  {7, 8},  {8, 7},
-
     {3, 8},  {8, 3},  {4, 8},  {8, 4},  {7, 0},  {0, 7},  {3, 0},  {0, 3},
-
     {4, 0},  {0, 4},  {8, 0},  {0, 8},  {7, 6},  {6, 7},  {3, 6},  {6, 3},
+
+
     {4, 6},  {6, 4},  {8, 6},  {6, 8},  {0, 6},  {6, 0},  {7, 5},  {5, 7},
+
+
     {3, 5},  {5, 3},  {4, 5},  {5, 4},  {8, 5},  {5, 8},  {0, 5},  {5, 0},
     {6, 5},  {5, 6},  {7, 1},  {1, 7},  {3, 1},  {1, 3},  {4, 1},  {1, 4},
     {8, 1},  {1, 8},  {0, 1},  {1, 0},  {6, 1},  {1, 6},  {5, 1},  {1, 5},
@@ -22,7 +23,6 @@ static const uint8_t pins[][2] = {
     {2, 10}, {10, 2}, {7, 9},  {9, 7},  {3, 9},  {9, 3},  {4, 9},  {9, 4},
 };
 
-// today's experiment AA:
 typedef struct ledBits_t {
     uint16_t dir : 1;
     uint16_t bits : 11;
@@ -51,7 +51,6 @@ static const ledBits_t bitArray[] = {
     {1, 0b00100001000}, //  8k, 3n   0x108
     {0, 0b00100001000}, //  8n, 3k
 
-      //  a9876543210
     {1, 0b00100010000}, //  8k, 4n   0x110
     {0, 0b00100010000}, //  8n, 4k
 
@@ -59,55 +58,93 @@ static const ledBits_t bitArray[] = {
     {0, 0b00010000001}, // this is why bit 0 still gets a '1'
     {1, 0b00010000001}, //  7k, 0n   0x081
 
+      //  a9876543210
+    {0, 0b00000001001}, //  3n, 0k   0x009
+    {1, 0b00000001001}, //  3k, 0n   0x009
+
+ // anode, cathode
+ // {4, 0},  {0, 4},  {8, 0},  {0, 8}, {7, 6},  {6, 7},  {3, 6},  {6, 3},
+
+    {0, 0b00000010001}, //  4n, 0k   0x011
+    {1, 0b00000010001}, //  4k, 0n   0x011
+
+    {0, 0b00100000001}, //  8n, 0k   0x000
+    {1, 0b00100000001}, //  8k, 0n   0x000
+
+      //  anode, cathode
+
+    {0, 0b00011000000}, //  7n, 6k   0x0c0
+    {1, 0b00011000000}, //  7k, 6n   0x0c0
+
+      //  a9876543210
+    {1, 0b00001001000}, //  6k, 3n   0x048
+    {0, 0b00001001000}, //  6n, 3k   0x048
+
+    // bookmark
+      //  anode, cathode
+//  {4, 6},  {6, 4},  {8, 6},  {6, 8},  {0, 6},  {6, 0},  {7, 5},  {5, 7},
+
+      //  a9876543210
+    {1, 0b00001010000}, //  0k, 0n   0x050
+    {0, 0b00001010000}, //  0n, 0k   0x050
+
+
+    {0, 0b00000000000}, //  0n, 0k   0x000
+    {1, 0b00000000000}, //  0k, 0n   0x000
+
     {1, 0x7AF}};
 
+void printBit(int newTestval, int testVal) {
+    bool unchanged = (newTestval == testVal);
+    bool bitSet = !unchanged;
+    if (!bitSet) { printf("0"); }
+    if (bitSet)  { printf("1"); }
+}
+
 int xmain() {
-
-    // arduino.cc array first:
-    // printf("sizeof(pins): %x\n", sizeof(pins));
-    // printf("sizeof(pins[0]): %x\n", sizeof(pins[0]));
-
     printf("sizeof(binPins): %x\n", sizeof(binPins));
 
     printf("sizeof(binPins[0]): %x\n", sizeof(binPins[0]));
-
-    // applied learning:
-    // static const ledBits_t bitArray[] = {{0x7FF, 0}, {0b111111, 1}, {0x7AF,
-    // 1}};
 
     printf("sizeof(bitArray): %x\n", sizeof(bitArray));
 
     printf("sizeof(bitArray[0]): %x\n\n", sizeof(bitArray[0]));
 
     int members = (sizeof(bitArray) / sizeof(bitArray[0]));
-    // sizeof(bitArray): c
-    // sizeof(bitArray[0]): 2
 
     for (int n = 0; n < members; n++) {
         if (n < 10) { printf(" "); } // left pad
         printf("bitArray[%d].dir:  %d    ", n, bitArray[n].dir);
         if (n < 10) { printf(" "); } // left pad
-        printf("bitArray[%d].bits: 0x%03x   \n", n, bitArray[n].bits);
+
+        int test = bitArray[n].bits ;
+
+        int ppnewTest = test >> 1 ; // shift all bits one place to the right.
+        if (ppnewTest == 99999) { printf(""); } // nop
+
+        printf("bitArray[%d].bits:  0b", n);
+        int newTest = 0;
+        newTest = test & 0x3FF; printBit(newTest, test); // bit 10    0b01111111111
+        newTest = test & 0x5FF; printBit(newTest, test); // bit  9    0b10111111111
+        newTest = test & 0x6FF; printBit(newTest, test); // bit  8    0b11011111111
+        newTest = test & 0x77F; printBit(newTest, test); // bit  7    0b11101111111
+        newTest = test & 0x7BF; printBit(newTest, test); // bit  6    0b11110111111
+        newTest = test & 0x7DF; printBit(newTest, test); // bit  5    0b11111011111
+        newTest = test & 0x7EF; printBit(newTest, test); // bit  4    0b11111101111
+        newTest = test & 0x7F7; printBit(newTest, test); // bit  3    0b11111110111
+        newTest = test & 0x7FB; printBit(newTest, test); // bit  2    0b11111111011
+        newTest = test & 0x7FD; printBit(newTest, test); // bit  1    0b11111111101
+        newTest = test & 0x7FE; printBit(newTest, test); // bit  0    0b11111111110
+        printf("  0x%03x\n", bitArray[n].bits);
     }
     printf("\n");
-
-    // printf("static const ledBits_t bitArray[] = {\n");
-
-    // printf("    {0, 0x7FF},         {1, 0b111111},\n");
-    // printf("    {1, 0b01000010000}, // LED array pins 9 and 4, inverted (9 "
-           // "low; 4 high)\n");
-    // printf("    {0, 0b10000000001}, {1, 0b10000000001},\n");
-    // printf("    {1, 0x7AF}};\n");
-
-    printf("}; // Thu 20 Jul 02:23:31 UTC 2023\n\n");
-
     return 0;
 }
 
 int main() {
     int result = xmain();
     printf("xmain() result: \n\n", result);
-    printf("Thu 20 Jul 03:03:02 UTC 2023\n\n");
+    printf("Thu 20 Jul 05:50:35 UTC 2023\n\n");
     return 0;
 }
-;
+
